454d1a694f35b5c4eb5ab195d3897fe2a9d9a971
devnote-dev <devnote.dev75@gmail.com>
2023-07-12 22:26:43 +0100

    feat: implement data append

--- src/linked.zig
+++ src/linked.zig
@@ -10,6 +10,29 @@ pub fn LinkedList(comptime T: type) type {
 
         len: usize,
         head: ?*Node,
+
+        pub fn append(self: *Self, data: T) void {
+            if (self.head) |head| {
+                var node = head;
+                while (node.tail != null) {
+                    node = node.tail.?;
+                }
+
+                node.tail = &Node{
+                    .data = data,
+                    .head = node,
+                    .tail = null,
+                };
+            } else {
+                self.head = &Node{
+                    .data = data,
+                    .head = null,
+                    .tail = null,
+                };
+            }
+
+            self.len += 1;
+        }
     };
 }
 
@@ -17,4 +40,15 @@ test "linked list" {
     const expect = @import("std").testing.expect;
 
     try expect(LinkedList(i32) == LinkedList(i32));
+
+    var nums = LinkedList(i32){
+        .head = null,
+        .len = 0,
+    };
+
+    nums.append(2);
+    nums.append(4);
+    nums.append(6);
+
+    try expect(nums.len == 3);
 }
