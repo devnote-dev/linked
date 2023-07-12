808adb1b7ad4141c45c8866284d8c6f85142588f
devnote-dev <devnote.dev75@gmail.com>
2023-07-12 23:42:31 +0100

    feat: attempt to implement index

--- src/linked.zig
+++ src/linked.zig
@@ -1,3 +1,5 @@
+const print = @import("std").debug.print;
+
 pub fn LinkedList(comptime T: type) type {
     return struct {
         const Self = @This();
@@ -10,6 +12,21 @@ pub fn LinkedList(comptime T: type) type {
 
         len: usize,
         head: ?*Node,
+
+        pub fn index(self: *Self, pos: usize) ?T {
+            if (pos < 0 or pos > self.len - 1 or self.head == null) return null;
+            if (pos == 0) return self.head.?.data;
+
+            var node = self.head.?;
+            var i: usize = 0;
+            while (i <= pos) {
+                print("\ni: {}\n", .{i});
+                node = node.tail.?;
+                i += 1;
+            }
+
+            return node.data;
+        }
 
         pub fn append(self: *Self, data: T) void {
             if (self.head) |head| {
@@ -51,4 +68,14 @@ test "linked list" {
     nums.append(6);
 
     try expect(nums.len == 3);
+
+    var value = nums.index(0);
+    try expect(value != null);
+    try expect(value.? == 2);
+
+    value = nums.index(1);
+    try expect(value != null);
+    try expect(value.? == 4);
+
+    try expect(nums.index(3) == null);
 }
